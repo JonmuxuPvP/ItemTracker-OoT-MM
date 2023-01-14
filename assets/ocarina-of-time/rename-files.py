db40ea0a9e9c1961c4370d15ca25eee15fb0df90
Jonmuxu <Jonmuxupvp@gmail.com>
2023-01-14 21:44:20 +0100

    Removed unnecessary file

--- assets/ocarina-of-time/rename-files.py
+++
@@ -1,9 +0,0 @@
-import os
-
-root = 'L:\\Jonmuxu\\Programming Languages\\JavaScript\\Item Tracker\\ItemTracker-OoT-MM\\assets\\ocarina-of-time'
-file_names = os.listdir(root)
-
-for file in file_names:
-    new_name = file.replace(' ', '-').replace('\'', '')
-    new_name = new_name.lower()
-    os.rename(f'{root}\\{file}', f'{root}\\{new_name}')
